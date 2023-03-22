5acc60af5976306836502080cfc1b6c8a4d7e59b
Bartosz Dobija <33185107+BartShoot@users.noreply.github.com>
2023-03-22 22:10:16 +0100

    Drugie okno jest tworzone na mniejszym ekranie

--- InfoKiosk/App.axaml.cs
+++ InfoKiosk/App.axaml.cs
@@ -1,8 +1,11 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Avalonia.Platform;
+using Avalonia.Win32;
 using InfoKiosk.ViewModels;
 using InfoKiosk.Views;
+using System.Linq;
 
 namespace InfoKiosk
 {
@@ -23,10 +26,21 @@ namespace InfoKiosk
                 };
                 var window = new MainWindow();
                 window.DataContext = new MainWindowViewModel();
+                Screen? smallerScreen = GetSmallerScreen();
+                window.Position = new PixelPoint(smallerScreen.Bounds.X, 0);
                 window.Show();
             }
 
             base.OnFrameworkInitializationCompleted();
+        }
+
+        private static Screen? GetSmallerScreen()
+        {
+            var screenImpl = new ScreenImpl();
+            var allScreens = screenImpl.AllScreens;
+            int lowestWidth = allScreens.Min(scr => scr.Bounds.Width);
+            var smallerScreen = allScreens.Where(scr => scr.Bounds.Width == lowestWidth).FirstOrDefault();
+            return smallerScreen;
         }
     }
 }
