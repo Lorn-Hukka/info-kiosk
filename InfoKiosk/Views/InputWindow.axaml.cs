ef8c63dca93e3a9f2daad372e1bdbb6387ca332b
Bartosz Dobija <33185107+BartShoot@users.noreply.github.com>
2023-03-23 21:17:09 +0100

    Dodanie przycisków do drugiego okna

---
+++ InfoKiosk/Views/InputWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace InfoKiosk.Views;
+
+public partial class InputWindow : Window
+{
+    public InputWindow()
+    {
+        InitializeComponent();
+    }
+}
