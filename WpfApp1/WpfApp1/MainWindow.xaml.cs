b8c8fbb9dc8ac870cdef2a1f45c58e2e03422991
Nikodem Nikiel <76944706+VeliverX@users.noreply.github.com>
2023-03-27 02:05:54 +0200

    Wstępne ustawienie aplikacji wielo-ekranowej

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿
+
+
+using System.Windows;
+using System.Windows.Forms;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+                    
+
+            InitializeComponent();
+
+            var screens = Screen.AllScreens;
+            var secondScreen = screens[1];
+            SecondWindow secondWindow = new SecondWindow()
+            {
+
+                Height = secondScreen.WorkingArea.Height,
+                Width = secondScreen.WorkingArea.Width,
+                Left = secondScreen.WorkingArea.Left,
+                Top = secondScreen.WorkingArea.Top,
+                WindowStyle = WindowStyle.None,
+                Topmost = true
+            };
+            
+            
+            secondWindow.Show();
+
+        }
+       
+
+
+    }
+}
---
+++ WpfApp1/WpfApp1/SecondWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Logika interakcji dla klasy SecondWindow.xaml
+    /// </summary>
+    public partial class SecondWindow : Window
+    {
+        public SecondWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Quit_Button_Clic(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+
+        //Loeaded event by okno poprawnie sie powiekszało na cały ekran, w przypadku gdy od razu chcemy je powiekszyc okno zostawało na pierwszym moniotrze 
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Maximized;
+        }
+    }
+}
