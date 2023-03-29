846a7d844aa77269a74f707160d50d29b7f471d0
Bartosz Dobija(BartShoot <dobija.bartosz@gmail.com>
2023-03-29 18:24:27 +0200

    MessageBus zmienia string w mainvm za pomocą inputvm

--- InfoKiosk/ViewModels/InputWindowViewModel.cs
+++ InfoKiosk/ViewModels/InputWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ReactiveUI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,13 @@ namespace InfoKiosk.ViewModels
 {
     public class InputWindowViewModel : ViewModelBase
     {
+        public void DawajCommand()
+        {
+            MessageBus.Current.SendMessage("Dawaj");
+        }
+        public void KlikajCommand()
+        {
+            MessageBus.Current.SendMessage("Klikaj");
+        }
     }
 }
--- InfoKiosk/ViewModels/MainWindowViewModel.cs
+++ InfoKiosk/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,23 @@
-﻿namespace InfoKiosk.ViewModels
+﻿using System;
+using ReactiveUI;
+using System.Reactive;
+using System.Reactive.Linq;
+
+namespace InfoKiosk.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting => "Welcome to Avalonia!";
+
+        public MainWindowViewModel() 
+        {
+            MessageBus.Current.Listen<string>().Subscribe(m => Greeting = m);
+        }
+
+        private string _message = "Welcome to Avalonia!";
+        public string Greeting 
+        { 
+            get => _message;
+            set { this.RaiseAndSetIfChanged(ref _message, value); } 
+        }
     }
 }
