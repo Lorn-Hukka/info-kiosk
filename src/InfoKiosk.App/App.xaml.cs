7d904dfdb0052b2e5fbb8a8d9a7ac191a2c3346a
Lukas <lucas.hamera@gmail.com>
2023-05-10 18:33:36 +0200

    Init project

---
+++ src/InfoKiosk.App/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using InfoKiosk.Presentation;
+using Prism.DryIoc;
+using Prism.Ioc;
+using Prism.Modularity;
+
+namespace InfoKiosk.App
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+        {
+        }
+    }
+}
