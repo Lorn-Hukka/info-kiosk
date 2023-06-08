568dd248f306bf86a07ccb105fd2c3351f783b75
Wojciech Kasolik <50033807+Lorn-Hukka@users.noreply.github.com>
2023-06-09 01:58:58 +0200

    Update HomeViewModel.cs
    - Kod odpowiadający za wyświetlenie odpowiedniego przywitania jak i daty i godziny

--- src/InfoKiosk.Modules.Home/ViewModels/HomeViewModel.cs
+++ src/InfoKiosk.Modules.Home/ViewModels/HomeViewModel.cs
@@ -1,12 +1,69 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Threading;
 
 namespace InfoKiosk.Modules.Home.ViewModels
 {
-    internal class HomeViewModel
+    public class HomeViewModel : INotifyPropertyChanged
     {
+        private string currentTime = "01.01.1970 00:00:00";
+        private string UserName;
+
+        public string WellcomeString {
+            get {
+
+                if (UserName == null || UserName == "")
+                {
+                    if (DateTime.Now.Hour < 17 && DateTime.Now.Hour > 4)
+                    {
+                        return "Dzień Dobry!";
+                    }
+                    else if (DateTime.Now.Hour > 17 && DateTime.Now.Hour < 23) {
+                        return "Dobry Wieczór!";
+                    }
+                    else
+                    {
+                        return "Dobranoc";
+                    }
+                }
+                else { return "Witaj, " + UserName + "!"; }
+
+            } 
+        }
+
+        public string CurrentTime
+        {
+            get { return currentTime; }
+            set
+            {
+                currentTime = value;
+                OnPropertyChanged("CurrentTime");
+            }
+        }
+
+        public HomeViewModel() {
+
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+
+        }
+
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            CurrentTime = DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss");
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public virtual void OnPropertyChanged(string prop)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
     }
 }
