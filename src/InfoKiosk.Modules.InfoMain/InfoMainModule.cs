2a9d77ff5c446755f28baf7f4526f8a7eabd977d
Nikodem Nikiel <76944706+VeliverX@users.noreply.github.com>
2023-06-14 00:30:53 +0200

    Dodanie widoku dla FAQ - WNoZ

--- src/InfoKiosk.Modules.InfoMain/InfoMainModule.cs
+++ src/InfoKiosk.Modules.InfoMain/InfoMainModule.cs
@@ -10,12 +10,13 @@ namespace InfoKiosk.Modules.InfoMain
         public void OnInitialized(IContainerProvider containerProvider)
         {
             var regionManager = containerProvider.Resolve<IRegionManager>();
-            //regionManager.RegisterViewWithRegion("MainRegion", typeof(InfoMainView));
-            //regionManager.RegisterViewWithRegion("MainRegion", typeof(InfoGeneralView));
-            //regionManager.RegisterViewWithRegion("MainRegion", typeof(InfoWBMiIView));
-            //regionManager.RegisterViewWithRegion("MainRegion", typeof(InfoWIMBiŚView));
-            //regionManager.RegisterViewWithRegion("MainRegion", typeof(InfoWZiTView));
+            regionManager.RegisterViewWithRegion("MainRegion", typeof(InfoMainView));
+            regionManager.RegisterViewWithRegion("MainRegion", typeof(InfoGeneralView));
+            regionManager.RegisterViewWithRegion("MainRegion", typeof(InfoWBMiIView));
+            regionManager.RegisterViewWithRegion("MainRegion", typeof(InfoWIMBiŚView));
+            regionManager.RegisterViewWithRegion("MainRegion", typeof(InfoWZiTView));
             regionManager.RegisterViewWithRegion("MainRegion", typeof(InfoWHSView));
+            regionManager.RegisterViewWithRegion("MainRegion", typeof(InfoWNoZView));
         }
 
         public void RegisterTypes(IContainerRegistry containerRegistry)
---
+++ src/InfoKiosk.Modules.InfoMain/ViewModels/InfoWNoZViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfoKiosk.Modules.InfoMain.ViewModels
+{
+    internal class InfoWNoZViewModel
+    {
+        public string Question => "Gdzie na naszej uczelni znajduje sie dziekanat";
+        public string Answer => "Dziekanat znajduje sie w budynku L, na pierwszym piętrze";
+
+        public string Question_2 => "Test zawijania w przypadku długich wiadomosci Test zawijania w przypadku długich wiadomosci Test zawijania w przypadku długich wiadomosci";
+    }
+}
